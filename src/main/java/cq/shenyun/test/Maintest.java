99edc256b3ec72a9d26fda25375842238e3b09d8
shenyun <luoyongsky@gmail.com>
2014-08-26 11:44:40 +0800

    java project file

---
+++ src/main/java/cq/shenyun/test/Maintest.java
@@ -0,0 +1,12 @@
+package cq.shenyun.test;
+
+public class Maintest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sdfdsf");
+	}
+}
